04003028d11f7d347c661dd56c131ec34b12d49f
NNbezGalki <137414368+NNbezGalki@users.noreply.github.com>
2024-05-21 16:01:48 +0500

    Add files via upload

---
+++ js1/burger_menu/script.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($) {
+	$('.header__burger').click(function(event) {
+		$('.header__burger,.header__menu').toggleClass('active'); // открываем/закрываем меню
+		$('body').toggleClass('lock');
+		// не даем странице прокручиваться при открытом меню
+	});
+});
